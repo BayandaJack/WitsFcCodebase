7a3f5639cb2e73be0d8fe6276b1e2a3e46b91387
BayandaJack <2669074@students.wits.ac.za>
2025-10-21 20:03:19 +0200

    refactor(formation): Change formation slightly

--- formation/Formation.py
+++ formation/Formation.py
@@ -2,7 +2,15 @@ import numpy as np
 
 def GenerateBasicFormation():
 
+    formation = [
+        np.array([-13, 0]),    # Goalkeeper
+        np.array([-7, -4]),  # Right Defender
+        np.array([-3, 4]),   # Left Defender
+        np.array([2, 0]),    # Forward Left
+        np.array([9, 0])      # Forward Right
+    ]
 
+    """
     formation = [
         np.array([-13, 0]),    # Goalkeeper
         np.array([-7, -2]),  # Left Defender
@@ -10,7 +18,7 @@ def GenerateBasicFormation():
         np.array([7, 1]),    # Forward Left
         np.array([12, 0])      # Forward Right
     ]
-
+    """
 
 
     # formation = [
